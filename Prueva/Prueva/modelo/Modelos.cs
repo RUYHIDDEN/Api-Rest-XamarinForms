ba4fbd3bc3ff0764a67bbc25e493310d906b105e
RUYHIDDEN <2519160082rgonzalezd@gmail.com>
2021-01-13 07:55:12 -0600

    Master
    Api-Xamarin

---
+++ Prueva/Prueva/modelo/Modelos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueva.modelo
+{
+    public class Modelos
+    {
+    }
+    public class User
+    {
+        public int Id_User { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ Prueva/Prueva/principal/AddUserView.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using Prueva.modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prueva.principal
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddUserView : ContentPage
+    {
+        public AddUserView()
+        {
+            InitializeComponent();
+            btnGuardar.Clicked += BtnGuardar_Clicked;
+        }
+
+        private async void BtnGuardar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(txtUsuario.Text))
+                {
+                    await DisplayAlert("Advertencia", "El campo Usuario es obligatorio", "OK");
+                }
+                else if (String.IsNullOrWhiteSpace(txtContrasena.Text))
+                {
+                    await DisplayAlert("Advertencia", "El campo Contraseña es obligatorio", "OK");
+                }
+                else if (String.IsNullOrWhiteSpace(txtTipo.Text))
+                {
+                    await DisplayAlert("Advertencia", "El campo Tipoa es obligatorio", "OK");
+                }
+                else
+                {
+                    var user = new User();
+                    user.Id_User = 0;
+                    user.Usuario = txtUsuario.Text;
+                    user.Contrasena = txtContrasena.Text;
+                    user.Tipo = txtTipo.Text;
+
+                    var request = new HttpRequestMessage();
+                    request.RequestUri = new Uri("http://192.168.1.69:3000/user");
+                    request.Method = HttpMethod.Post;
+                    request.Headers.Add("Accpet", "application/json");
+                    var payload = JsonConvert.SerializeObject(user);
+                    HttpContent c = new StringContent(payload, Encoding.UTF8, "application/json");
+                    request.Content = c;
+                    var client = new HttpClient();
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        await DisplayAlert("Notificación", "El usuario se a creado con éxito :" + txtUsuario.Text, "OK");
+                        await Navigation.PopAsync();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Notificación", "Error al conectar", "OK");
+                        await Navigation.PopToRootAsync();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Notificación", "Error al conectar", "OK");
+                await Navigation.PopToRootAsync();
+            }
+        }
+    }
+}
